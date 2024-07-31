72f59d7f66bda2e8378eb5c235c0c5842946049b
Elena Andreeva <elena.and.kam@gmail.com>
2024-07-31 10:13:52 +0300

    feat: add decision index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let reverseN = "";
+    let nToStr = n + "";
+
+    for (let i = 1; i <= nToStr.length; i++) {
+        reverseN += nToStr[nToStr.length - i];
+    }
+  return reverseN.replace('-', '');
+};
